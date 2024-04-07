56eb551276c8cad6d02d54a9f6fa82cf71fa758d
Sammi Husky <sammi-husky@live.com>
2024-04-06 19:55:01 -0700

    Update template to new version of sy_core

--- source/rel.cpp
+++ source/rel.cpp
@@ -4,34 +4,43 @@
 
 #include "sandbox.h"
 
-namespace Syringe {
+namespace Syringe
+{
+    const PluginMeta META = {
+        "Sandbox",
+        "Sammi",
+        Version("0.1.0"),
+        Version(SYRINGE_VERSION)};
 
-    extern "C" {
-    typedef void (*PFN_voidfunc)();
-    __attribute__((section(".ctors"))) extern PFN_voidfunc _ctors[];
-    __attribute__((section(".ctors"))) extern PFN_voidfunc _dtors[];
+    extern "C"
+    {
+        typedef void (*PFN_voidfunc)();
+        __attribute__((section(".ctors"))) extern PFN_voidfunc _ctors[];
+        __attribute__((section(".ctors"))) extern PFN_voidfunc _dtors[];
 
-    void _prolog();
-    void _epilog();
-    void _unresolved();
+        const PluginMeta *_prolog();
+        void _epilog();
+        void _unresolved();
     }
 
-    void _prolog()
+    const PluginMeta *_prolog()
     {
         // Run global constructors
-        PFN_voidfunc* ctor;
+        PFN_voidfunc *ctor;
         for (ctor = _ctors; *ctor; ctor++)
         {
             (*ctor)();
         }
 
         Sandbox::Init();
+
+        return &META;
     }
 
     void _epilog()
     {
         // run the global destructors
-        PFN_voidfunc* dtor;
+        PFN_voidfunc *dtor;
         for (dtor = _dtors; *dtor; dtor++)
         {
             (*dtor)();
